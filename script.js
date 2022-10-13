3608cf73f9c767bb0201600d240f7b693499f715
OKEREKE VINCENT <Vincentcode0@gmail.com>
2022-10-13 02:18:15 +0100

    Complete Nav

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const toggle = document.querySelector(".toggle");
-const nav = document.querySelector(".header-main");
-let navLinks = document.querySelectorAll(".link")
+const nav = document.querySelector(".nav-main");
+let lists = document.querySelectorAll(".btn");
+
 // const linkContainer = document.querySelector(".links")
 
 /* Toggle Nav */
@@ -8,3 +9,20 @@ toggle.addEventListener('click', () => {
   toggle.classList.toggle("show-nav");
   nav.classList.toggle("show-nav")
 });
+
+// lists.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     lists.forEach(Element => {
+//       Element.classList.remove("active")
+//       console.log("yo");
+//     })
+//     e.target.classList.add("active")
+//   })
+// })
+
+lists.forEach((btn) => {
+  btn.addEventListener("click", (element) => {
+    lists.forEach(link => link.classList.remove("active"));
+    element.target.classList.add("active")
+  })
+})
