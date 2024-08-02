8bc0af1eadbb3bc8ae1139365b779b8467204718
ACE <anthonychimaobi43@gmail.co>
2024-08-02 06:30:45 +0000

    Feat: toggle theme nav/hero

---
+++ components/atoms/themeToggle.tsx
@@ -0,0 +1,55 @@
+"use client";
+import Button from "./button";
+import {useEffect, useState} from "react";
+import {LightMode, DarkModeOutlined} from '@mui/icons-material';
+
+
+export default function ThemeToggle() {
+    const [darkMode, setDarkMode] = useState(true);
+
+    useEffect(() => {
+        const theme = localStorage.getItem("theme")
+        if (theme === "dark") setDarkMode(true)
+    }, [])
+
+
+    useEffect(() => {
+        if (darkMode) {
+            document.documentElement.classList.add('dark')
+            localStorage.setItem("theme", "dark")
+        } else {
+            document.documentElement.classList.remove('dark')
+            localStorage.setItem("theme", "light")
+        }
+    }, [darkMode])
+
+
+    return (
+        <div className="flex  items-center w-full max-w-[200px]">
+                    
+            <Button width="specs" size="md">Login </Button>
+
+        <div className="flex bg-[#fdfdfd] border-2 border-[#dcd5d5] dark:border-[#FFFFFF33]/30 w-full max-w-[3.4rem] h-[1.6rem] ml-[1.2rem] rounded-[1.5rem] overflow-hidden p-[0.2rem] dark:bg-green-900"
+        onClick={()=> setDarkMode(!darkMode)}> 
+
+                <LightMode className="!text-base ml-[2px] place-self-center z-10 text-[#FFFFFF] dark:text-[#FFFFFF66]/40" />
+
+                <div 
+                className={`flex items-center justify-center place-content-center place-items-center bg-[#07C589] h-[1.2rem] w-full max-w-[1.2rem] rounded-full place-self-center transform z-0 transition-transform duration-300 ${darkMode ? "ml-[1.7rem]" :  "mr-[1.8rem]"} absolute `}
+                >
+                    {/* sx={{color:"#fdfdfd",width:"65%"}} */}
+
+                    
+
+                </div> 
+
+                   < DarkModeOutlined  className="!text-base mr-[2.2px] place-self-center z-10 text-[#1E1E1E99] dark:text-[#FFFFFF] ml-3 "/> 
+
+        </div>
+
+
+
+        </div>
+    )
+}
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,17 +6,12 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors:{
-        dark:"#232A3C",
-        medium:"#293245"
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
