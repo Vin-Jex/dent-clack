8026b34c650f0bc3fded12f6d5a57b25b80a22a2
Bulut Yerli <bulutyerli@gmail.com>
2024-10-01 23:51:51 +0300

    fix hero section images (#83)
    * fix hero section iamges
    
    * fix size of sections, flexbox of hero section
    
    * fix hero image size, left section responsiveness
    
    * make hero images bigger

--- components/LandingPage/Hero.tsx
+++ components/LandingPage/Hero.tsx
@@ -1,33 +1,33 @@
-
-import Button from "../atoms/Button";
-import HeroMap from "./heroMap";
+import Button from '../atoms/Button';
+import HeroMap from './heroMap';
 
 // import Daniel from "c/daniel.jpg"
 // import { heroImgLinks } from '@/constants/index'
 
 export default function Hero() {
   return (
-    <section className={`pb-[13rem] flex flex-col  sm:flex-row `}>
-      <div className=' flex flex-col mt-[8rem]'>
-        <h1 className='text-[4rem] font-bold leading-[6rem] mb-4'>
+    <section
+      className={`pb-[13rem] mt-[4rem] flex flex-col gap-20 sm:flex-row justify-between items-center`}
+    >
+      <div className=" flex flex-col">
+        <h1 className="text-[2rem] xs:text-[3rem] md:text-[4rem] font-bold sm:leading-relaxed md:leading-[6rem] mb-4">
           We Make Your <br />
-          <span className='text-[#07C589]'>Smile</span> Our Priority.
+          <span className="text-[#07C589]">Smile</span> Our Priority.
         </h1>
 
-        <p className='text-[1rem] mb-[2rem]'>
+        <p className="text-md sm:text-base md:text-xl mb-[2rem]">
           At DentClack, we are dedicated to one thing - your smile. With <br />
           over 10 years of professional experience and more than 100 <br />
-          carefully selected dental practices, we are uniquely equipped to{" "}
+          carefully selected dental practices, we are uniquely equipped to{' '}
           <br />
           provide personalized treatment and restore your desired smile.
         </p>
 
-        <Button width='auto' size='height'>
+        <Button width="auto" size="height">
           Book an Appointment
         </Button>
       </div>
-
-      <div className=' flex flex-row flex-wrap border sm:ml-[4.5rem] w-full max-w-[28rem] h-[28rem] top-[6rem] sm:mt-[0] rounded-[8em] overflow-hidden relative'>
+      <div className="w-full max-w-[38rem] h-full overflow-hidden relative grid grid-cols-2 grid-rows-2 gap-4 md:gap-7">
         {/* <Image src= {heroImgLinks.name} className="w-full h-ful object-cover" alt="jh" /> */}
 
         {/* <Image src={Nwanboje}  className="w-full h-ful object-cover" alt="jh"></Image> */}
