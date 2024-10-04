fe626877267afb6efc441955a37357ec56352571
Neel Mishra <neelmishra9125@gmail.com>
2024-10-04 12:04:02 +0530

    SignIn page done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider, SignIn, SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <ClerkProvider>
+      {/* <SignIn/> */}
       <body className={inter.className}>{children}</body>
+      </ClerkProvider>
     </html>
   );
 }
