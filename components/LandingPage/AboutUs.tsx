acb7b9e85ea99b81aaa0b4daa12ef52363c843d4
Anthony <149781461+Anthony-Chima@users.noreply.github.com>
2024-10-27 20:10:32 +0000

    Fix:escape single quote, correct light & dark toggle button

--- components/LandingPage/AboutUs.tsx
+++ components/LandingPage/AboutUs.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function AboutUs() {
   return (
-    <section className="pb-24 justify-between space-y-8 justify-items-center text-center ">
+    <section className="pb-24 justify-between space-y-8 justify-items-center text-center">
       
       <div className="space-y-1 justify-items-center w-full">
          <p className="text-[#07C589]
@@ -12,7 +12,7 @@ export default function AboutUs() {
         dark:text-[#FFFFFF]">Who we are</p>
       <p className="text-[#1E1E1E8C] 
         text-sm
-        dark:text-[#FFFFFFCC] w-[72%]">For your dental health, trust us to provide the best treatment. We'll guide you from your first consultation to lifelong maintenance. We believe a beautiful smile enhances your quality of life and aim to achieve the best results in a fun, relaxed environment.</p>
+        dark:text-[#FFFFFFCC] w-[72%]">For your dental health, trust us to provide the best treatment. We&apos;ll guide you from your first consultation to lifelong maintenance. We believe a beautiful smile enhances your quality of life and aim to achieve the best results in a fun, relaxed environment.</p>
       </div>
      
       
