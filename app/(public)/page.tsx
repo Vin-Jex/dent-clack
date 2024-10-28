e6c6b87205fcbb86e89b5c4b8330e69ed09dac32
Oyinda08 <146204325+Oyinda08@users.noreply.github.com>
2024-10-28 08:52:40 -0700

    Feat: Create Authentication layout #84 (#110)
    * Feat: Create Authentication layout #84
    
    * Fix: Bugfix

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,53 +1,49 @@
 import Hero from "@/components/LandingPage/Hero";
 import About from "@/components/LandingPage/about";
 import AboutUs from "@/components/LandingPage/AboutUs";
-import Services from "@/components/LandingPage/Services"
+import Services from "@/components/LandingPage/Services";
 import Specialist from "@/components/LandingPage/Specialist";
 import Testimonials from "@/components/LandingPage/Testimonials";
 
-const styles = {
-  paddingX: "px-8 sm:px-[4rem]",
-};
-
 export default function Home() {
   return (
-    <main className='font-poppins flex-col text-[#1E1E1E] h-full'>
+    <main className="font-poppins flex-col text-[#1E1E1E] h-full">
       <div>
         <div
-          className='bg-primaryLight dark:text-[#FFFFFF] px-2 md:px-12 dark:bg-primaryDark'
-          id='home'
+          className="bg-primaryLight dark:text-[#FFFFFF] px-2 md:px-12 dark:bg-primaryDark"
+          id="home"
         >
           <Hero />
         </div>
 
         <div
           className={`bg-[#FFFFFF] dark:bg-[#1c2622] px-2 md:px-12`}
-          id='about'
+          id="about"
         >
           <About />
         </div>
-        
+
         <div
           className={`bg-[#FFFFFF] dark:bg-[#1c2622] px-2 md:px-12`}
-          id='about'
+          id="about"
         >
           <AboutUs />
         </div>
         <div
           className={`bg-[#FFFFFF] dark:bg-[#1c2622] px-2 md:px-12`}
-          id='Services'
+          id="Services"
         >
           <Services />
         </div>
         <div
           className={`bg-primaryLight dark:bg-primaryDark px-2 md:px-12`}
-          id='Specialist'
+          id="Specialist"
         >
           <Specialist />
         </div>
         <div
           className={`bg-[#FFFFFF] dark:bg-[#1c2622] px-2 md:px-12`}
-          id='Testimonials'
+          id="Testimonials"
         >
           <Testimonials />
         </div>
